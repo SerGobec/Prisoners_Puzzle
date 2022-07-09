85d98f2e7ccf4f69e06d008fc5cef809a391280d
SerGobec <48455848+SerGobec@users.noreply.github.com>
2022-07-09 16:28:56 +0300

    init commit

---
+++ Prisoners_Puzzle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prisoners_Puzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
